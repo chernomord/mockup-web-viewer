f35aaf33799c526d4f29a0f7faf4a89ec72639ac
horlet <horlet@gmail.com>
2015-08-21 06:18:14 +0300

    pre-animate stage

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var mwvApp = angular.module("mwvApp", ['ngRoute']);
+var mwvApp = angular.module("mwvApp", ['ngRoute','ngAnimate']);
 
 mwvApp.config(['$routeProvider',
   function($routeProvider) {
@@ -56,10 +56,54 @@ mwvApp.controller('project', function($scope, dataLoader) {
 mwvApp.controller('projectMockup', ['$scope', '$routeParams', 'dataLoader',
   function($scope, $routeParams, dataLoader) {
 
-    dataLoader.mainRes('mockups.json').success(function(data) {
+    dataLoader.mainRes('mockups.json').success(function load (data) {
       $scope.mockupFile = 
         data.mockupGroups[$routeParams.gr].mockups[$routeParams.it].file; 
+      image = new Image();
+      image.src = $scope.mockupFile;
+      $scope.$watch('mockupH', function(newVal, oldVal) {
+        $scope.myprop = function() {
+            return { height: $scope.mockupH + 'px' };
+        };
+      }, true);
+      image.onload = function() {
+          $scope.mockupH = image.naturalHeight; 
+          console.log($scope.mockupH);
+          $scope.$apply();
+        };
+
+
     });
     $scope.mockupName = $routeParams.mockupName;
-
 }]);
+
+
+mwvApp.directive('resize', function ($window) {
+    return function (scope, element, attr) {
+
+        var w = angular.element($window);
+        scope.$watch(function () {
+            return {
+                'h': window.innerHeight, 
+                'w': window.innerWidth
+            };
+        }, function (newValue, oldValue) {
+            /*console.log(newValue, oldValue);*/
+            scope.windowHeight = newValue.h;
+            scope.windowWidth = newValue.w;
+
+            scope.resizeWithOffset = function (offsetH) {
+                scope.$eval(attr.notifier);
+                return { 
+                    'height': (newValue.h - offsetH) + 'px'                    
+                };
+            };
+
+        }, true);
+
+        w.bind('resize', function () {
+            scope.$apply();
+        });
+    }
+}); 
+
